a2aee59fcc48a0e47d907f444742c73c51148a48
Yudhi234 <94178164+Yudhi234@users.noreply.github.com>
2024-09-11 01:36:47 +0530

    Login Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import ToggleColorMode from "./componets/ToggleColorMode";
+import Views from "./componets/views";
 
  
 function App() {
   return (
    <>
     <ToggleColorMode/>
+    <Views/>
    </>
   );
 }
--- src/componets/Login.jsx
+++ src/componets/Login.jsx
@@ -1,8 +1,33 @@
- 
+ import {Vstack,ButtonGroup,FormControl,FormLabel,Button,FormErrorMessage, VStack, Input, Heading} from "@chakra-ui/react"
  
  
  const Login=()=>{
-     return <></>
+     return <VStack as="form" w={{base:"90%",md:"500px"}} m="auto"
+     justify="center" h="100vh" spacing="1rem"
+     > 
+<Heading>Login</Heading>
+
+      <FormControl>
+        <FormLabel fontSize="lg">Username</FormLabel>
+        <Input size="lg" name="username" placeholder="Enter your username" autoComplete="off"/>
+        <FormErrorMessage>Invalid username</FormErrorMessage>
+     
+      </FormControl>
+    
+     <FormControl>
+       
+     <FormLabel fontSize="lg">Password</FormLabel>
+        <Input size="lg" name="password" placeholder="Enter your password" autoComplete="off"/>
+        <FormErrorMessage>Invalid password</FormErrorMessage>
+     
+     </FormControl>
+
+
+     <ButtonGroup pt="1rem">
+        <Button colorScheme="green" type="submit"> Login</Button>
+        <Button>Create Account</Button>
+     </ButtonGroup>
+     </VStack>
  };
 
 
