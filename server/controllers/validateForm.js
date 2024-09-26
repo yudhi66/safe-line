0e89aa9a97af04a1fef25bfca8396c71374e3e34
yudhi66 <yudhivikram536@gmail.com>
2024-09-26 21:12:34 +0530

    code refactor

--- server/controllers/validateForm.js
+++ server/controllers/validateForm.js
@@ -1,14 +1,17 @@
 import formSchema  from "../../common/index.js"
 
 
-const validateForm =(req,res)=>{
+const validateForm =(req,res,next)=>{
     const formData=req.body;
     formSchema.validate(formData).catch(err=>{
-         res.status(422).send(), 
-         console.log(err.errors);
+        res.status(422).send(); 
     }).then(valid =>{
           if(valid){
              console.log("Form is good");
+             next();
+          }else{
+            res.status(422).send(); 
+            
           }
     });
 }
--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,74 +1,12 @@
 import express from 'express';
 import validateForm from '../controllers/validateForm.js';
+import { handleLogin,attemptLogin,attemptSignup } from '../controllers/authController.js';
 const router =express.Router();
-import pool from  "../db.js"
-import bcrypt from "bcrypt"
 
-router.route("/login")
-.get(async (req, res) => {
-  if (req.session.user && req.session.user.username) {
-    res.json({ loggedIn: true, username: req.session.user.username });
-  } else {
-    res.json({ loggedIn: false });
-  }
-})
-
-
-
-.post(  async (req,res)=>{
-   validateForm(req,res);
-
-    
-   
-
-
-   const potentialLogin=await pool.query("SELECT id,username,passhash FROM users WHERE  username=$1",[req.body.username]);
-
- if(potentialLogin.rowCount >0){
-    const isSamePass = await bcrypt.compare(req.body.password,potentialLogin.rows[0].passhash);
-    if(isSamePass){
-        //login
-        req.session.user={
-            username:req.body.username,
-            id: potentialLogin.rows[0].id,
-          }
-          console.log("Session after login:", req.session);
-          res.json({ loggedIn: true, username: req.body.username });
-   
-        }else{
-        //not good login
-        console.log("not good");
-        res.json({loggedIn:false,status:"Wrong username or password"});
-      }
- }else{
-    console.log("not good");
-     res.json({loggedIn:false,status:"Wrong username or password"});
-
- }
-
-
-
-
-})
+import  authController   from "../controllers/authController.js" 
+router.route("/login").get(handleLogin).post(validateForm,  attemptLogin);
   
-router.post("/signup",async (req,res)=>{
-    validateForm(req,res);
-    const existingUser =await  pool.query("SELECT username from users WHERE username=$1",[req.body.username]);
-     if(existingUser.rowCount===0){
-      //register
-      const  hashedPass=await bcrypt.hash(req.body.password,10);
-      const newUserQuery=await pool.query("INSERT INTO users(username,passhash) values($1,$2) RETURNING id,username",[req.body.username,hashedPass]);
-      req.session.user={
-        username:req.body.username,
-        id:newUserQuery.rows[0].id,
-      }
-      res.json({loggedIn:true,username:req.body.username});
-     }else{
-      res.json({loggedIn:false,status:"Username Taken"});
- 
-     }
-
- })
+router.post("/signup",validateForm,  attemptSignup);
 
 
 
