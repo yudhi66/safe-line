1f0fa29fc4438d4e8ffbe2bab3e427f5c411083d
yudhi66 <yudhivikram536@gmail.com>
2024-09-29 01:33:46 +0530

    Tab panels

--- client/src/componets/home/Home.jsx
+++ client/src/componets/home/Home.jsx
@@ -1,13 +1,13 @@
 import {Grid,GridItem,Tabs} from "@chakra-ui/react"
 import Sidebar from "./sidebar";
-
+import Chat from "./Chat";
 const HomePage = () => {
     return     <Grid templateColumns="repeat(10, 1fr)" h="100vh" as={Tabs}  >
     <GridItem colSpan="3" borderRight="1px solid gray">
       <Sidebar />
     </GridItem>
     <GridItem colSpan="7">
-     
+     <Chat/>
     </GridItem>
   </Grid>
   };
