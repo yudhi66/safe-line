ee60836991714780ad00f61553bbd1d78ee501c7
yudhi66 <yudhivikram536@gmail.com>
2024-10-02 09:38:56 +0530

    modal validation

--- client/src/componets/home/AddFriendModal.jsx
+++ client/src/componets/home/AddFriendModal.jsx
@@ -1,33 +1,55 @@
-import { Modal, ModalBody,Button, ModalContent, ModalFooter, ModalHeader, ModalOverlay, ModalCloseButton } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@chakra-ui/modal";
+import { Button, ModalOverlay } from "@chakra-ui/react";
+import * as Yup from 'yup';
+import { Form, Formik } from "formik";
 import TextField from "../TextField";
-import { Formik ,Form} from "formik";
 
-const AddFriendModal=({isOpen,onClose})=>{
-    return <Modal isOpen={isOpen} onClose={onClose} isCentered>
-  <ModalOverlay/>
-  <ModalContent>
-       
-       <ModalHeader>Add a friend</ModalHeader>
-       <ModalCloseButton/>
-
-       <Formik initialValues={{friendName:""}} onSubmit={(values,actions)=>{
-        alert(JSON.stringify(values,null,2));
-        actions.resetForm();
-       }}>
-        <Form>
-       <ModalBody> 
-        <TextField label="Friends name" placeholder="Enter friends username..." autoComplete="off" name="friendName"/>
-       </ModalBody>
-       <ModalFooter>
-       <Button colorScheme='blue'   onClick={onClose} type="submit">
-              Submit
-            </Button>
-
-       </ModalFooter>
-       </Form>
-       </Formik>
-  </ModalContent>
+const localFriendSchema = Yup.object({
+  friendName: Yup.string()
+    .required("Username required")
+    .min(6, "Invalid username!")
+    .max(28, "Invalid username!"),
+});
+const AddFriendModal = ({ isOpen, onClose }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Add a friend!</ModalHeader>
+        <ModalCloseButton />
+        <Formik
+          initialValues={{ friendName: "" }}
+          onSubmit={values => {
+            onClose();
+          }}
+          validationSchema={localFriendSchema}
+        >
+          <Form>
+            <ModalBody>
+              <TextField
+                label="Friend's name"
+                placeholder="Enter friend's username.."
+                autoComplete="off"
+                name="friendName"
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" type="submit">
+                Submit
+              </Button>
+            </ModalFooter>
+          </Form>
+        </Formik>
+      </ModalContent>
     </Modal>
-}
+  );
+};
 
 export default AddFriendModal;
